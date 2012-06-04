d98347f5c43e8fbaa28c69e8691bca3b94e424e7
Ivan <imillan@terra.es>
2012-06-04 17:04:16 +0200

    header changes

--- header.php
+++ header.php
@@ -5,23 +5,17 @@
 <!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
   <meta charset="utf-8">
-
   <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
-
   <meta name="viewport" content="width=device-width">
-
+  <meta name="creator" content="Linxspace.net">
   <?php roots_stylesheets(); ?>
-
   <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
-
   <script src="<?php echo get_template_directory_uri(); ?>/js/libs/modernizr-2.5.0.min.js"></script>
   <script src="<?php echo get_template_directory_uri(); ?>/js/libs/css3-mediaqueries.js"></script>
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
-
   <?php roots_head(); ?>
   <?php wp_head(); ?>
-
 </head>
 
 <body <?php body_class(roots_body_class()); ?>>
